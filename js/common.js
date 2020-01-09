78eec51c5531504a138f368aa7d1f7373f912d1e
gpf <992093924@qq.com>
2020-01-09 21:19:32 +0800

    dsc:20200109

--- js/common.js
+++ js/common.js
@@ -23,14 +23,26 @@ function backMainMenu(){
     menu.style.display = 'block'
 }
 
+// 显示/隐藏头部下拉栏目
+function showTitleSelect(num){
+    // console.log(num)
+    $('#tittleSelect').toggleClass('show')
+    $('#tittleSelect').toggleClass('noshow')
+    $('#tittleSelect ul li').find("span").removeClass('menu-active')
+    $('#tittleSelect ul li').eq(num).find("span").addClass('menu-active')
+
+}
+
 $(document).ready(function() {
 //点击折叠时运行该函数
     $('.collapse').on('hide.bs.collapse show.bs.collapse', function (e) {
         e.stopPropagation()
         var $this = $(this)
-
         $this.prev('.question-tittle').find("span").toggleClass("span_plus");
         $this.prev('.question-tittle').find("span").toggleClass("span_reduce");
+
+        $this.prev('.menu-question-tittle').find("span").toggleClass("span_plus");
+        $this.prev('.menu-question-tittle').find("span").toggleClass("span_reduce");
     });
 });
 
